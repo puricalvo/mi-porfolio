bab57c50225d6635341d2951ad5f8724a5f8be4c
puri calvo <puri_adrian@hotmail.com>
2026-04-15 21:52:40 +0200

    Limpieza de dependencias

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
-// @ts-check
 import { defineConfig } from "astro/config";
-import tailwindv4 from "@tailwindcss/vite"; // Importamos el plugin de Tailwind 4
+import tailwindv4 from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  // 1. Pon la URL de tu perfil de GitHub
+  site: "https://puricalvo.github.io",
+
+  // 2. IMPORTANTE: Pon el nombre de tu repositorio precedido de una barra /
+  // Si tu repo se llama mi-porfolio, debe quedar así:
+  base: "/mi-porfolio",
+
   vite: {
-    plugins: [tailwindv4()], // Activamos el plugin en Vite
+    plugins: [tailwindv4()],
   },
 });
